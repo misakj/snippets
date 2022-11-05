c68414c6620d51b8136223754581735b77c242d2
Jack <misakj34@gmail.com>
2022-11-05 09:06:19 -0600

    string and number review

---
+++ strings.js
@@ -0,0 +1,34 @@
+// const string = "The revolution will not be televised."
+        // const badString = string;
+        // console.log(badString);
+        
+        // String literal
+        // const name = "Chris";
+        // backtick
+        // const greeting = `Hello, ${name}`;
+        // console.log(greeting);
+
+        // join variables
+        // const one = "Hello, ";
+        // const two = "how are you?";
+        // const joined = `${one}${two}`; // backticks
+        // console.log(joined); // Hello, how are you?
+
+        // Concatenation with button
+        // const button = document.querySelector("button");
+
+        // function greet() {
+        //     const name = prompt("What is your name?");
+        //     alert(`Hello ${name}, nice to see you!`);
+        // }
+
+        // button.addEventListener("click", greet);
+
+        // Expressions in strings
+        const song = "Fight the Youth";
+        const score = 9;
+        const highestScore = 10;
+        const output = `I like the song ${song}. I give it a score of ${
+            (score / highestScore) * 100
+        }%.`;
+        console.log(output);
